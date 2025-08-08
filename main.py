e526031f164619020445604391274d8363c76b14
Henry Fong <foongsy@gmail.com>
2025-08-08 22:03:08 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
+import chainlit as cl
+import httpx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = OpenAIModel(
+    'google/gemini-2.5-flash-lite',
+    provider=OpenAIProvider(
+        base_url='https://openrouter.ai/api/v1',
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        http_client=httpx.AsyncClient(verify=False)
+    ),
+)
+
+simple_agent = Agent(
+    model=model,
+    system_prompt=(
+        'Please answer everything in japanese'
+    ),
+)
+
+@cl.on_chat_start
+def on_start():
+    cl.user_session.set("agent", simple_agent)
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    agent = cl.user_session.get("agent")
+    response = agent.run_sync(message.content)
+    await cl.Message(content=response.output).send()
